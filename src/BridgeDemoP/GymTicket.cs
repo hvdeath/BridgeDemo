cd97ee4e9a41f56ef1fa1a10ea73b60f7b5b78af
Viktor Halász <hvd29@hotmail.com>
2021-01-16 09:36:16 +0100

    Task 1: Base + OneTimeTicket, MonthlyTicket

---
+++ src/BridgeDemoP/GymTicket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BridgeDemoP
+{
+    public abstract class GymTicket 
+    {
+        protected GymTicket(DateTime purchaseTime)
+        {
+            PurchaseTime = purchaseTime;
+        }
+        public abstract string Name { get;  }
+        public abstract double Price { get;}
+        public DateTime PurchaseTime { get; set; }
+        
+        public abstract DateTime GetExpirationDate();
+    }
+
+
+    public class OneTimeGymTicket: GymTicket
+    {
+        public OneTimeGymTicket(DateTime purchaseTime): base(purchaseTime)
+        {
+        }
+        public override string Name => "Daily gym ticket";
+        public override double Price => 1500;
+
+        public override DateTime GetExpirationDate()
+        {
+            return PurchaseTime.AddHours(3);
+        }
+    }
+
+    public class MonthlyGymTicket : GymTicket
+    {
+        public MonthlyGymTicket(DateTime purchaseTime) : base(purchaseTime)
+        {
+        }
+        public override string Name => "Monthly ticket";
+        public override double Price => 15000;
+
+        public override DateTime GetExpirationDate()
+        {
+            return PurchaseTime.AddDays(30);
+        }
+    }
+}
---
+++ src/BridgeDemoP/Program.cs
@@ -0,0 +1,36 @@
+﻿using BridgeDemoP;
+using System;
+
+
+Console.WriteLine("Hello World Bridge Demo!");
+
+
+PrintDetails(new OneTimeGymTicket(DateTime.Now));
+PrintDetails(new MonthlyGymTicket(DateTime.Now));
+
+
+static void PrintDetails(GymTicket ticket)
+{
+    Console.WriteLine($"Gym: {ticket.Name}");
+    Console.WriteLine($"Price: {GetPrice(ticket)}");
+    Console.WriteLine($"Valid for: {GetValidFor(ticket)}");
+
+    Console.WriteLine();
+}
+
+static string GetPrice(GymTicket ticket)
+{
+    return $"{ticket.Price:0.00} HUF";
+}
+
+static string GetValidFor(GymTicket ticket)
+{
+    DateTime? expirationDate = ticket.GetExpirationDate();
+
+    if (expirationDate == null)
+        return "Forever";
+
+    TimeSpan timeSpan = expirationDate.Value - DateTime.Now;
+
+    return $"{timeSpan.Days}d {timeSpan.Hours}h {timeSpan.Minutes}m";
+}
